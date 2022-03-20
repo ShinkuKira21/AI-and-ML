908c939ae8412ff2e436e2e49314da382e217225
Edward Patch <edwardpatch1@gmail.com>
2022-03-20 00:01:15 +0000

    Update to KMeans

--- Assignment 1/KMeans/KMeans/ClusterTools/kmeans.cpp
+++ Assignment 1/KMeans/KMeans/ClusterTools/kmeans.cpp
@@ -1,9 +1,21 @@
 #include "tools.h"
-
-bool KMeans(std::vector<std::vector<MTools::Vector2D<size_t>>>* dataPoints, std::vector<MTools::Vector2D<float>> kClusterPoints)
+#include <iostream>
+bool CTools::KMeans::KMeans(std::vector<std::vector<MTools::Vector2D<size_t>>>* clusterPoints, std::vector<MTools::Vector2D<float>>* kClusterPoints)
 {
-	// loving the for_each function xd
-	for_each(dataPoints->begin(), dataPoints->end(), Assignment);
+	// clusterPoints should be zero first time through.
+	std::vector<std::vector<MTools::Vector2D<size_t>>> cpyClusterPoints(*clusterPoints);
+
+	std::cout << "Size of clusterPoints: " << clusterPoints->size() << std::endl;
+	std::ranges::for_each(cpyClusterPoints.begin(), cpyClusterPoints.end(), [clusterPoints, kClusterPoints] (const std::vector<MTools::Vector2D<size_t>> clusterPoint)
+	{
+		std::ranges::for_each(clusterPoint.begin(), clusterPoint.end(), [clusterPoints, kClusterPoints](const MTools::Vector2D<size_t> dataPoint)
+		{
+			int selector = Assignment(dataPoint, *kClusterPoints);
+
+		});
+	});
+
+	std::cout << "New size of clusterPoints: " << clusterPoints->size() << std::endl;
 	/* Whilst a condition is true - loop til change
 		// assign dataPoints to clusterPoints (spreadsheet)
 		// Calculate 
@@ -12,8 +24,17 @@ bool KMeans(std::vector<std::vector<MTools::Vector2D<size_t>>>* dataPoints, std:
 	return false;
 }
 
-int Assignment(MTools::Vector2D<size_t> point, std::vector<MTools::Vector2D<float>> kClusterPoints)
+int CTools::KMeans::Assignment(MTools::Vector2D<size_t> point, std::vector<MTools::Vector2D<float>> kClusterPoints)
 {
 	// find the best cluster point
+	/* TODO:
+	 * loop through each point and find the lowest cluster point
+	*/
+	std::vector<std::vector<MTools::Vector2D<float>> comparisons;
+	std::ranges::for_each(kClusterPoints.begin(), kClusterPoints.end(), [comparisons](const MTools::Vector2D<float> kPoint))
+	{
+
+	});
+	return 1;
 
 }
