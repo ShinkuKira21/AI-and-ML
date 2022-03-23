9033c0a253a577ef020c0133c181007c267011f4
Edward Patch <edwardpatch1@gmail.com>
2022-03-23 12:02:40 +0000

    Added Matlab App

--- Assignment 1/KMeans/KMeansMatlab/KMeans.m
+++ Assignment 1/KMeans/KMeansMatlab/KMeans.m
@@ -1,7 +1,1 @@
-% Learnt from the CLib from Mathworks Tutorials
-x = clib.coms.XPlusY(2, 3);
-y = clib.coms.RandomNumber();
-xy = clib.coms.A;
-xy.x = x;
-xy.y = y;
-z = clib.coms.XY(xy);
+test = readtable("cvs/KCluster.csv")
