3acbad44945416c59e7b075d126b84f5c7342a5f
ShinkuKira21 <crazygaming055@gmail.com>
2022-03-29 12:06:47 +0100

    Finalized the DFS Algorithm

--- Assignment 1/DFS/Source Code/Deep First Search/main.cpp
+++ Assignment 1/DFS/Source Code/Deep First Search/main.cpp
@@ -17,29 +17,29 @@ namespace Manager
 	}
 }
 
-void CreateMatlabCVS(Manager::Manager* manager)
+// ~find comment 1.0 for this to make sense | comment 3.0 - remove manager->positions.pop() | add depth to argument :) and move depth into the comment 2.0 area |
+CVSTools::CVSHandler* CreateMatlabCVS()
 {
 	CVSTools::CVSProperties* properties = new CVSTools::CVSProperties();
 	properties->col = 4;
 	properties->headings = new std::string[4]{ "D",  "N", "X", "Y" };
 
-	CVSTools::CVSHandler* cvs = new CVSTools::CVSHandler(properties, { "../DFSMatlab/cvs/", "DFS" });
+	return new CVSTools::CVSHandler(properties, { "../DFSMatlab/cvs/", "DFS" });
+}
 
-	size_t depth = manager->positions.size() - 1;
-	while(!manager->positions.empty())
-	{
+void MatlabRows(Manager::Manager* manager, CVSTools::CVSHandler* cvs, size_t depth)
+{
+	// comment 1.0 problem with this is that when this stage begins after the results are fetched ~find comment 2.0
+	//size_t depth = manager->positions.size() - 1;
+	//while(!manager->positions.empty())
+	//{
 		cvs->CVSParser(0, std::to_string(depth).c_str());
 		cvs->CVSParser(1, manager->positions.top().node);
 		cvs->CVSParser(2, std::to_string(manager->positions.top().x).c_str());
 		cvs->CVSParser(3, std::to_string(manager->positions.top().y).c_str());
-		manager->positions.pop();
-		depth--;
-	}
-
-	cvs->CVSStart();
-	delete cvs;
-
-	std::cout << "Generated Necessary Data Files for Matlab:" << std::endl << "Go to the DFSMatlab folder and open DFS Matlab application";
+		//manager->positions.pop();
+		//depth--;
+	//}
 }
 
 int main(int argc, char **argv)
@@ -61,19 +61,24 @@ int main(int argc, char **argv)
 	const char nodeToFind = Functions().TextInput("Enter Node to Find (Char): ")[0];
 	Manager::Manager manager;
 	Manager::DisplayBoard(config);
-	
+
+	// Initialise CVS
+	CVSTools::CVSHandler* cvs = CreateMatlabCVS();
 	manager.positions = StackController::InitialiseStack();
 
 	// Once the start path is found, now find the next objects
 	manager.direction = DepthFirstSearch::ELeft;
 	AddToStack(&manager.positions, FindStartObject(config, manager.direction));
 
-
+	size_t depth = 1;
  	if(manager.positions.top().node == 83)
 		while (!manager.positions.empty())
 		{
+			// comment 2.0
 			printf("N: %c X: %zi | Y: %zi | Last Direction Enum: %i | Current Direction Enum: %i\n", manager.positions.top().node, manager.positions.top().x, manager.positions.top().y, manager.positions.top().pLastDirection, manager.direction);
-
+			// move the CreateMatlabCVS here, ~find comment 3.0
+			// creates a CVS file for Matlab
+			MatlabRows(&manager, cvs, depth);
 			if (nodeToFind == manager.positions.top().node) break;
 			if (manager.positions.top().node == 70) break;
 
@@ -81,10 +86,12 @@ int main(int argc, char **argv)
 			// Ascii to Text | 42 -> *
 			if ((searchPosition = NextCurrentObject(config, manager.positions.top(), &manager.direction)).node != '\0') AddToStack(&manager.positions, searchPosition);
 			else RemoveFromStack(&manager.positions);
+			depth++;
 		}
 
-	// creates a CVS file for Matlab
-	CreateMatlabCVS(&manager);
+	cvs->CVSStart();
+	delete cvs;
+	std::cout << "Generated Necessary Data Files for Matlab:" << std::endl << "Go to the DFSMatlab folder and open DFS Matlab application";
 
 	EmptyStack(&manager.positions);
 
